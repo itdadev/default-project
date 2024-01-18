031e02fb0d4570b400068b3d6bb4e509c62b6a4c
lilly <lilly@itdadev.com>
2024-01-18 18:37:26 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {BrowserRouter} from "react-router-dom";
+
+import {ReactQueryDevtools} from "react-query/devtools";
+import {QueryProvider} from "@/lib/react-query/QueryProvider";
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import GlobalStyles from "@/assets/styles/GlobalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* NOTE: react query setting */}
+      <QueryProvider>
+        {/* NOTE: Globally resetting CSS styles*/}
+        <GlobalStyles />
+
+        {/* NOTE: react query dev tools */}
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right"/>
+
+        <App/>
+      </QueryProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
